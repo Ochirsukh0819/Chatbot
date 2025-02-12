74c1adaa9849d0e469863b5a9ab297f86c845653
Ochirsukh0819 <oojoo20020819@gmail.com>
2025-02-12 22:26:49 +0800

    fix: update code

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -3,5 +3,5 @@ import { expect, test } from "@playwright/test"
 test("has title", async ({ page }) => {
   await page.goto("./")
 
-  await expect(page).toHaveTitle(/UNLMTDAI Frontend Template/)
+  await expect(page).toHaveTitle(/Simple chatbot application/)
 })
